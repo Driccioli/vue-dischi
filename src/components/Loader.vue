f1cff6d99027d89f6e6e15db62e8b6d1a1d44ed8
Driccioli <driccioli53@gmail.com>
2021-07-14 17:49:55 +0200

    Aggiunto bonus

---
+++ src/components/Loader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Caricamento in corso...
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Loader",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/colors.scss";
+div{
+    text-align: center;
+    font-size: 25px;
+    font-weight: bold;
+    color: $album-title;
+}
+</style>
